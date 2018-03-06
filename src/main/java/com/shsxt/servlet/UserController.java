e9e15cc5ab41e6fc7d6c17a5c0ad0d129c3725f0
yonghumingbucunzai123 <1176608772@qq.com>
2018-03-06 10:25:25 +0800

    首次提交代码

---
+++ src/main/java/com/shsxt/servlet/UserController.java
@@ -0,0 +1,23 @@
+package com.shsxt.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/user")
+public class UserController extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("yyy");
+	}
+	
+}
